4613418d8f7925b48ccd99354d8712224a8aab66
Sukrit Khera <sukrit007@gmail.com>
2016-04-05 20:02:32 -0700

    Revert "Feature coalescing support"

--- test/integration/api.js
+++ test/integration/api.js
@@ -35,10 +35,9 @@ var factoryConfig = {
     push: {
       cmd: 'true',
       timeout: 10000
-    }
-  },
-  concurrentJobs: 1,
-  coalescingPeriod: 5000
+    },
+    concurrentJobs: 1
+  }
 };
 
 var MINIMUM_BUILD_REQUEST = {
@@ -146,38 +145,6 @@ describe('Image Factory - REST API', function () {
           expect(response.statusCode).to.equal(400);
           expect(body.id).to.not.exist;
           done();
-        }
-      );
-    });
-
-    it('should create a new Job and coalesce a second Job when POST /job twice with identical payloads within the coalescing period', function (done) {
-
-      async.times(
-        2,
-        function (n, next) {
-          request.post(
-            BASE_URL + '/job',
-            { json: MINIMUM_BUILD_REQUEST },
-            function (err, response, body) {
-              next(err);
-            }
-          );
-        },
-        function (err, results) {
-          request.get(
-            BASE_URL + '/job',
-            function (err, response, body) {
-              expect(err).to.not.exist;
-              expect(response.statusCode).to.equal(200);
-              expect(response.headers).to.include.keys('link');
-              expect(response.headers['link']).to.contain('</_schema/job-list>; rel="describedBy"');
-              expect(response.headers).to.include.keys('content-type');
-              expect(response.headers['content-type']).to.contain('application/vnd.sh.melt.cdp.if.job-list.v1+json');
-              var results = JSON.parse(body);
-              expect(results).to.have.length(1);
-              done();
-            }
-          );
         }
       );
     });
@@ -379,6 +346,7 @@ describe('Image Factory - REST API', function () {
       }
     });
 
+
   });
 
   describe('GET /job/{id}/log', function () {
@@ -420,7 +388,7 @@ describe('Image Factory - REST API', function () {
     });
 
   });
-  
+
   describe('GET /job', function () {
 
     it('should return a list of known Jobs when requesting GET /job', function (done) {
@@ -428,17 +396,9 @@ describe('Image Factory - REST API', function () {
       async.times(
         5,
         function (n, next) {
-          // Need to differentiate the requests because of coalescing
-          var owner = "fake-owner-" + n.toString();
-          var repo = "fake-repo-" + n.toString();
           request.post(
             BASE_URL + '/job',
-            { 
-              json: {
-                owner: owner,
-                repo: repo
-              }
-            },
+            { json: FULL_BUILD_REQUEST },
             function (err, response, body) {
               next(err);
             }
